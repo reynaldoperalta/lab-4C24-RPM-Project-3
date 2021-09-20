cb6ed00fc674a3b2c3082005dc88e42c8c4fdd44
reynaldoperalta <reynaldopema14@gmail.com>
2021-09-20 00:31:06 -0500

    second change no error

---
+++ src/com/tecsup/lab9/git/model/student.java
@@ -0,0 +1,20 @@
+package com.tecsup.lab9.git.model;
+
+public class student {
+	public static void main( String[] args )
+    {
+        String nombre = "Reynaldo";
+        int ciclo_estudio = 4;
+        System.out.println("Acciones del estudiante");
+        
+        System.out.println("Mi nombre es: " + nombre);
+        System.out.println("Estoy en el ciclo: " + ciclo_estudio);
+        
+        if(nombre == "Reynaldo") {
+        	System.out.println("Hola: " + nombre);
+        	
+        }
+        
+    }
+
+}
